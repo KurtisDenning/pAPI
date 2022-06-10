040b07825ebbde69443a827e09f175b24fedd448
Oliver Schweikert <oliver.schweikert@pm.me>
2022-06-10 13:17:06 +1200

    Update Swagger to use remote host

--- backend/docs/docs.go
+++ backend/docs/docs.go
@@ -295,7 +295,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "papi-project.herokuapp.com",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "pAPI",
