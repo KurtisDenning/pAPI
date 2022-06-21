87c5d2c309c424b3d8c325b38f9c4d09a1d5ca1f
Canofcode <kurtisdenning@gmail.com>
2022-06-21 19:37:40 +1200

    fixed nav and footer links

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -3,19 +3,20 @@ import { Link } from "react-router-dom";
 import { Box, Flex, Heading } from "@chakra-ui/react";
 
 const Footer = () => {
-
   function scrollToTop() {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
-    })
+    });
   }
 
   return (
     <Box bg={"#222222"}>
       <Flex h={"33vh"} alignItems={"center"} justifyContent={"center"}>
-        <Heading color={"white"} fontSize={["2rem", "3rem"]} >
-          <Link onClick={scrollToTop} to={"/pAPI"}>pApi</Link>
+        <Heading color={"white"} fontSize={["2rem", "3rem"]}>
+          <Link onClick={scrollToTop} to={"/"}>
+            pApi
+          </Link>
         </Heading>
       </Flex>
     </Box>
