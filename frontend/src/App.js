61817310d4314ef80dd74a79d8946c3a78c09863
Canofcode <kurtisdenning@gmail.com>
2022-06-21 19:09:27 +1200

    404 fix attempt 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -110,16 +110,6 @@ function App() {
         <Nav />
         <Routes>
           <Route
-            path="*"
-            element={
-              <>
-                <Header query={query} setQuery={setQuery} />
-                <Divider />
-                {isLoading()}
-              </>
-            }
-          />
-          <Route
             path="/pAPI"
             element={
               <>
@@ -132,6 +122,16 @@ function App() {
           <Route path="/pAPI/API">
             <Route path=":id" element={<APIPage />} />
           </Route>
+          <Route
+            path="*"
+            element={
+              <>
+                <Header query={query} setQuery={setQuery} />
+                <Divider />
+                {isLoading()}
+              </>
+            }
+          />
         </Routes>
         <Footer />
       </Router>
